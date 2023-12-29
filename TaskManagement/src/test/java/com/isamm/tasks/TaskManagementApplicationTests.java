781aa083b114fdfe4ee3196a75d0d72783f42d7b
mohamed.habaieb <mohamed.habaieb@gaconnecte.com>
2023-12-29 13:09:29 +0100

    run test in dockerfile backend

--- TaskManagement/src/test/java/com/isamm/tasks/TaskManagementApplicationTests.java
+++ TaskManagement/src/test/java/com/isamm/tasks/TaskManagementApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = TaskManagementApplication.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class TaskManagementApplicationTests {
 
 	@Autowired
